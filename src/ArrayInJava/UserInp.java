e395fe55ae3b23b16ecabe35e44bdfa27bb3b344
Jay Raval <jayraval30801@gmail.com>
2023-02-24 12:16:01 +0530

    '-'

---
+++ src/ArrayInJava/UserInp.java
@@ -0,0 +1,15 @@
+package ArrayInJava;
+import java.util.Scanner;
+
+public class UserInp {
+
+		public static void main(String[] args) {
+			String a,b;
+				Scanner sc =  new Scanner(System.in);
+				System.out.println("Enter a");	
+				a = sc.nextLine(); // next => word nexLine => Line word
+				System.out.println("Enter b");
+				b= sc.nextLine();
+				
+		}
+}		
